e8e4fff8f9aeb89ac4715610c0d19889a07783f3
serjoga <serjoga@serjoga-MS-7309>
2013-01-21 00:11:44 +0200

    customers№211###

--- query/check_customers.php
+++ query/check_customers.php
@@ -20,6 +20,7 @@ mysql_free_result($result);
  
 
 if(count($new_customers)){
+    $GLOBALS['cu'] = count($new_customers);
     ?>
 <script type="text/javascript">
     alert("В сторонних базах зарегистрировались новые клиенты!\n\t\t Таблица будет дополнена!")
@@ -53,11 +54,17 @@ function _insertNewPersons($arr, $db){
     
     foreach ($arr as $value) {
         
-        _addToBases(_isWho(array('uid'=>$value[user_id],'db'=>$value[db_data_id],'tablename'=>$value[tablename])), $db);
+        $cu = _isWho(array('uid'=>$value[user_id],'db'=>$value[db_data_id],'tablename'=>$value[tablename]));
         
-//        $query = "INSERT INTO `customer` (user_id,role,surname,name,patronymic,email,phone,tablename,db_data_id) VALUES ($value[user_id],'$value[role]','$value[surname]','$value[name]','$value[patronymic]','$value[email]','$value[phone]','$value[tablename]',$value[db_data_id])";
-//        
-//        mysql_query($query) or die("Помилка ".  mysql_error());
+        $cu[role_name] = $value[role];
+        
+        $cu[db_data_id] = $value[db_data_id];
+        
+        $query = "INSERT INTO `customer` (user_id,role,surname,name,patronymic,email,phone,tablename,db_data_id) VALUES ($value[user_id],'$value[role]','$value[surname]','$value[name]','$value[patronymic]','$value[email]','$value[phone]','$value[tablename]',$value[db_data_id])";
+        
+        mysql_query($query) or die("Помилка ".  mysql_error());
+        
+//         _addToBases($cu, $db);
     }
     
     return NULL;
@@ -84,63 +91,123 @@ function _isWho($men){
 
 function _addToBases($customer, $db){
     
-   $need_tables = array('customer'=>array(),'users'=>array());
+    
+    if($customer[role_name] == 'Заказчик'){
+       $need_tables = 'customer'; 
+    }else{
+       $need_tables = 'users'; 
+    }
    
    ksort($customer);
+   
+   $new_customers = array();
     
     foreach ($db as $val) {
-        
-        if($customer [db_data_id] != $val[db_id]){
+      
+        if($customer[db_data_id] != $val[db_id]){
         
             mysql_close();
             mysql_connect($val[addr], $val[login], $val[password]);
             mysql_select_db($val[db_name]);
             mysql_query ("SET NAMES $val[charset]");
-            
-            foreach ($need_tables as  $key => $value) {
+
                 
-                if(mysql_table_seek($key, $val[db_name])){
-                        $need_tables[$key][$val[db_name]]=array();
-                        
-                        $result = mysql_query("SHOW COLUMNS FROM $key");
-                        $tmp = array();
-                        while ($fn = mysql_fetch_assoc($result)){
-                            
-                            array_push($tmp, $fn[Field]);
-                        }
-                        sort($tmp);
-                        $need_tables[$key][$val[db_name]] = $tmp;
-                        adaptingFields($customer, $tmp,$val[db_id]);
-                       
+            if(mysql_table_seek($need_tables, $val[db_name])){
+
+                $result = mysql_query("SHOW COLUMNS FROM $need_tables");
+                $tmp = array();
+                while ($fn = mysql_fetch_assoc($result)){
+
+                    array_push($tmp, $fn[Field]);
                 }
-            }
+                sort($tmp);
+
+                $customer[table] = $need_tables;
+                
+                if($need_tables == 'users')$customer[role] = getRoleID($customer[role_name]);
+                
+                unset($customer[role_name]);
+                unset($customer[user_id]);
+                $normo_customer = adaptingFields($customer, $tmp,$val[db_id]);
+                
+                array_push($new_customers, $normo_customer);
+                
+//                _insert($normo_customer,$need_tables);
+                
+                
+
+            } 
         }
     }
         include '../query/connect.php';
+        
+//        foreach ($new_customers as $value) {
+//             if(_insert($value, 'customer') > 0)$GLOBALS[cu]-1;
+//                if($GLOBALS[cu] == 0){
+//                    
+//                                         ?>
+<!--<script type="text/javascript">
+    document.location.href = "index.php?act=main";
+</script>-->
+//<?php
+//
+//          }
+        
+//    }
 
 }
 
+function _insert($customer, $tablename){
+    $str_filds = '';
+    $str_values = "";
+    $where = '';
+    foreach ($customer as $key => $value) {
+        $str_filds .= "$key,";
+        $str_values .= "'$value',";
+        if($key == 'pwd' OR $key == 'secret_key'){
+            $where = "$key = '$value'";
+        }
+    }
+    
+    $result = mysql_query("SELECT Count(id) FROM $tablename WHERE $where");
+    
+    $row = mysql_fetch_row($result);
+    
+    if($row[0] == 0){
+         $str_filds = substr($str_filds, 0, strlen($str_filds)-1);
+    $str_values = substr($str_values, 0, strlen($str_values)-1);
+    $query = "INSERT INTO `$tablename` ($str_filds) VALUES ($str_values)";
+    mysql_query($query);
+    }
+    
+   
+    
+    return mysql_insert_id();
+}
+
 function adaptingFields($customer,$fields,$db_id){
-    echo "<br>USER ->>      ";
-    print_r($customer);
+//    echo "<br>USER ->>      ";
+//    print_r($customer);
     $equal = array();
     $out_customer = array('db_id'=>$db_id);
+
     foreach ($fields as $value) {
-        foreach ($customer as $key => $var) {
-            if($key == $value)$out_customer[$value]=$var;
-//            if(ereg("mail", $value)){
-//                $out_customer[$value]=$var;
-//            }
-//            if($value == 'role' && $key == 'role'){
-//                $out_customer[$value] = getRoleID($var);
-//            }
-            if($value == 'id')unset ($out_customer[$value]);
-//            if($value == 'pwd')
+        if($customer[$value]){
+            $out_customer[$value]=$customer[$value];
         }
-    }  
-    echo "<br>adapting =>> ";
-    print_r($out_customer);
-    echo "<br>:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::<br>";
+
+        foreach ($customer as $key => $var) {
+            if(ereg("mail", $value) && ereg("mail", $key)){
+                $out_customer[$value]=$var;
+            }
+        }
+   }
+   
+    unset($out_customer[role_name]);
+    unset($out_customer[db_id]);
+    unset($out_customer[id]);
+    
+    return $out_customer;
 }
 
 function getRoleID($name){
