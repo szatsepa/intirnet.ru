a9b647708fe7a555abd1232724f62d6d054ccde7
serjoga <serjoga@serjoga-MS-7309>
2013-01-20 11:19:26 +0200

    customers№2

--- query/check_customers.php
+++ query/check_customers.php
@@ -1,32 +1,22 @@
 <?php
-$db_tables = array();
-
-$db_sort = array('0'=>'','1'=>'ORDER BY `name`','2'=>'ORDER BY `surname`','3'=>'ORDER BY `role`','4'=>'ORDER BY `creation_time`');
-
-$sort = '';
-
-foreach ($db_data as $value){
-    
-    $tmp = str_word_count($value[db_query], 1);
-    
-    array_push($db_tables, $tmp[2]);
-}
+$tmp_array = array();
 
 $new_customers = array();
 
-$query = "SELECT c.id, t.name, t.patronymic, t.surname, t.role, t.email, t.phone, t.password, t.db_data_id FROM `tmp_U1` AS t LEFT JOIN `customer` AS c USING(password) WHERE c.password IS NULL";
+$query = "SELECT t.user_id, t.name, t.patronymic, t.surname, t.role, t.email, t.phone, t.tablename, t.db_data_id FROM `tmp` AS t LEFT JOIN `customer` AS c USING(id) WHERE c.id IS NULL";
 
-$result = mysql_query($query) or die($query);
+$result = mysql_query($query) or die($query." -> ".  mysql_error());
 
 if($result){
     while($var = mysql_fetch_assoc($result)){
 
-        array_push($new_customers, $var);
+        array_push($tmp_array, $var);
     }
 }
 
+$new_customers = array_unique($tmp_array);
 
- mysql_free_result($result);
+mysql_free_result($result);
  
 
 if(count($new_customers)){
@@ -35,30 +25,39 @@ if(count($new_customers)){
     alert("В сторонних базах зарегистрировались новые клиенты!\n\t\t Таблица будет дополнена!")
 </script>
 <?php
+        _insertNewPersons($new_customers);
+}
 
-    foreach ($new_customers as $value) {
+$customers = _allPersons(NULL);
+
+function _allPersons($sort){
+    
+    $customers = array();
+
+    $query = "SELECT * FROM `customer` $sort";
+
+    $result = mysql_query($query) or die($query);
+
+    if(mysql_numrows($result)!=0){
+        while ($var = mysql_fetch_assoc($result)){
+            array_push($customers, $var);
+        }
+    }
+
+    mysql_free_result($result);
+    
+    return $customers;
+}
+
+function _insertNewPersons($arr){
+    
+    foreach ($arr as $value) {
         
-        $query = "INSERT INTO `customer` (role,surname,name,patronymic,email,phone,password,db_data_id) VALUES ('$value[role]','$value[surname]','$value[name]','$value[patronymic]','$value[email]','$value[phone]','$value[password]',$value[db_data_id])";
+        $query = "INSERT INTO `customer` (user_id,role,surname,name,patronymic,email,phone,tablename,db_data_id) VALUES ($value[user_id],'$value[role]','$value[surname]','$value[name]','$value[patronymic]','$value[email]','$value[phone]','$value[tablename]',$value[db_data_id])";
         
         mysql_query($query) or die("Помилка ".  mysql_error());
     }
+    
+    return NULL;
 }
-
-$customers = array();
-
-if(isset($attributes[sort])){
-    $sort = $db_sort[intval($attributes[sort])];
-}
-
-$query = "SELECT * FROM `customer` $sort";
-
-$result = mysql_query($query) or die($query);
-
-if(mysql_numrows($result)!=0){
-    while ($var = mysql_fetch_assoc($result)){
-        array_push($customers, $var);
-    }
-}
-
-mysql_free_result($result);
 ?>
---
+++ query/check_other_bases.php
@@ -0,0 +1,156 @@
+<?php
+mysql_query("DROP TABLE IF EXISTS `tmp`");
+
+$query = "CREATE TABLE IF NOT EXISTS `tmp` (
+        `id` int(11) NOT NULL auto_increment,
+        `user_id`  int(11) NOT NULL,
+        `name` varchar(255) character set utf8 collate utf8_bin NOT NULL,
+        `surname` varchar(255) character set utf8 collate utf8_bin NOT NULL,
+        `patronymic` varchar(255) character set utf8 collate utf8_bin NOT NULL,
+        `role` varchar(255) character set utf8 collate utf8_bin NOT NULL,
+        `phone` varchar(255) character set utf8 collate utf8_bin NOT NULL,
+        `email` varchar(255) character set utf8 collate utf8_bin NOT NULL,
+        `tablename` varchar(255) character set utf8 collate utf8_bin NOT NULL,
+        `db_data_id`  int(11) NOT NULL,
+        PRIMARY KEY  (`id`)
+        ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;";
+
+$result = mysql_query($query) or die($query);
+
+$query = "SELECT `id` AS db_id, `db_name`, `login`, `password`, `addr`, `charset` FROM `db_data`";
+
+$result = mysql_query($query) or die($query);
+
+$odb_tables = array();
+
+while($var = mysql_fetch_assoc($result)){
+    array_push($odb_tables, $var);
+}
+
+mysql_free_result($result);
+
+mysql_close();
+
+$persons_on_tmp = array();
+
+// получаем список таблиц в бд если есть таблицы customer or users забиваем в массив
+
+foreach ($odb_tables as $value) {
+    
+    mysql_connect("$value[addr]","$value[login]","$value[password]") or die ("Ошибка 1");
+    
+    mysql_select_db($value[db_name]);
+    
+    mysql_query ("SET NAMES $value[charset]");
+    
+    $table_list = mysql_query("SHOW TABLES");
+    
+    while($var = mysql_fetch_assoc($table_list)){
+        if(($var['Tables_in_'.$value[db_name]]) === 'customer' OR ($var['Tables_in_'.$value[db_name]]) === 'users'){
+            
+             $tablename =   $var['Tables_in_'.$value[db_name]];
+             
+             $tmp = array_merge($persons_on_tmp, _personsData($tablename,$value));
+                         
+             $persons_on_tmp = $tmp;
+             
+        }
+    }
+
+ mysql_close();
+    
+}
+
+
+include '../query/connect.php';
+
+_insertToTmp($persons_on_tmp);
+
+function _personsData($tablename,$array){
+    
+    unset($array[id]);
+    
+    $array[tablename] = $tablename;
+    
+    $tmp = array();
+    
+    $result = mysql_query("SELECT * FROM `$tablename`");
+    
+    while($var = mysql_fetch_assoc($result)){
+        
+        unset($var[user_id]);
+        
+        $tmp_arr = array_merge(_uniKeyEmail($var), $array);
+
+        $tmp_arr[role] = _getRole($tmp_arr[role]);
+        
+        array_push($tmp, $tmp_arr);
+    }
+    return $tmp;
+}
+
+function _uniKeyEmail($arr){
+    $tmp = array();
+    foreach ($arr as $key => $value) {
+        
+        if($key != 'e_mail' && $key != 'id'){
+           $tmp[$key] = $value; 
+        }else if($key == 'e_mail'){
+           $tmp[email] = $value; 
+        }else if($key == 'id'){
+           $tmp[user_id] = $value; 
+        }
+    }
+    return _uniKeyPWD($tmp);
+}
+
+function _uniKeyPWD($arr){
+    $tmp = array();
+    foreach ($arr as $key => $value) {
+        
+        if($key != 'secret_key' && $key != 'pwd'){
+           $tmp[$key] = $value; 
+        }else if($key == 'secret_key'){
+           $tmp[user_password] = $value; 
+        }else if($key == 'pwd'){
+            $tmp[user_password] = $value;
+        }
+    }
+    unset($tmp[password]);
+    unset($tmp[login]);
+    unset($tmp[pwd]);
+    
+    return $tmp;
+}
+
+function _getRole($id_role){
+    
+    $role = 'Заказчик';
+    
+    $id = intval($id_role);
+    
+    if($id){
+            $result = mysql_query("SELECT name FROM `roles` WHERE `id` = $id");
+            $row = mysql_fetch_row($result);
+            $role = $row[0];
+     }
+    return $role;
+}
+
+function _insertToTmp($arr){
+    
+    $message = 1;
+    
+    foreach ($arr as $value) {
+        
+        $query = "INSERT INTO `tmp` (user_id,surname,name,patronymic,email,phone,db_data_id,tablename,role) VALUES ('$value[user_id]','$value[surname]','$value[name]','$value[patronymic]','$value[email]','$value[phone]',$value[db_id],'$value[tablename]','$value[role]')";
+    
+        mysql_query($query) or die($query);
+        
+     }
+     if(count($arr != mysql_insert_id()))$message = NULL;
+     
+    return $message;
+}
+
+?>
