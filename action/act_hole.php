5752894b631ce5ea28ecdd5b912436cdc71c2fdd
serjoga <serjoga@serjoga-MS-7309>
2013-04-19 23:42:56 +0300

    remake##

--- action/act_hole.php
+++ action/act_hole.php
@@ -7,9 +7,38 @@ foreach ($_POST as $key => $value){
     $query .= "$key=$value&";
 }
 
-$query = substr($query, 0, (strlen($query)-1));
+//строка с POST данными
+$data = substr($query, 0, (strlen($query)-1));
+//
+//$somepage = file_get_contents("http://{$_POST['db_host']}/hole/hole.php?$query");
+//
+//echo $somepage;
 
-$somepage = file_get_contents("http://{$_POST['db_host']}/hole/hole.php?$query");
+/*
+*POST запрос при помощи file_get_contents
+*автор: nc_soft
+*29.08.07
+*/
+ 
+/*задать необходимые заголовки можно в массиве http, для примера посланы 2 заголовка
+User-Agent и Connection
+*/
+ 
+//строка с POST данными
+//$data='a=1&b=2';
+ 
+//задаем контекст
+$context = stream_context_create(
+array(
+        'http'=>array(
+                        'header' => "User-Agent: Brauzer 2/0\r\nConnection: Close\r\n\r\n",
+                        'method' => 'POST',
+                        'content' => $data                
+                     )
+    )
+);
+ 
+$contents = file_get_contents("http://{$_POST['db_host']}/hole/hole.php", false ,$context);
 
-echo $somepage;
+echo $contents;
 ?>
